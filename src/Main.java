59728abde5cea3971717fc14b5360cd4705b34e4
Oriol <oriolfontcu@gmail.com>
2024-02-05 12:21:03 +0100

    Menu v0.1.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
+
+    /* TODO:
+        - Menú
+        - Donar de alta nous clients Roger
+        - Donar de alta nous mecànics
+        - Introduir vehicle
+        - Ftitxa de reparació
+        - Poder modificar reparacións
+     */
+
+    public static int Menu(){
+        Scanner input = new Scanner(System.in);
+
+        int menuItem = 0;
+        do {
+            System.out.println("TALLER DE REPARACIÓ DE VEHICLES");
+            System.out.println("[1] Donar d’alta nou client");
+            System.out.println("[2] Donar d’alta nou mecànic");
+            System.out.println("[3] Introduir nou vehicle");
+            System.out.println("[4] Crear fitxa de nova reparació");
+            System.out.println("[5] Sortir");
+            System.out.println("Sel·lecciona una opció: ");
+
+            if (input.hasNextInt()){
+                menuItem = input.nextInt();
+
+            }
+
+        } while(menuItem!=5);
+
+        return menuItem;
+    }
     public static void main(String[] args) {
+        int menuItem = Menu();
+        System.out.println(menuItem);
 
     }
 }
